6fec89c234e31f037c0c4eca780843ce10b2030e
Samtray <sammychurro@gmail.com>
2022-09-13 19:35:14 -0700

    Primer commit

---
+++ SKA-Backend/src/main/kotlin/com/churris/SKABackend/SkaBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.churris.SKABackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SkaBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<SkaBackendApplication>(*args)
+}
