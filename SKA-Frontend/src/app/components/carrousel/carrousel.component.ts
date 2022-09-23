6e0712ee86f0dc0303827d877b43d15662eea12e
Gtorrecillas103 <gtorrecillas103@gmail.com>
2022-09-22 18:53:00 -0700

    imagen redonda y matricula

--- SKA-Frontend/src/app/components/carrousel/carrousel.component.ts
+++ SKA-Frontend/src/app/components/carrousel/carrousel.component.ts
@@ -18,19 +18,63 @@ const alumnos =
   [
     {
       name: "Alcaraz Ibarra Ahmed Fabian",
-      img: "https://randomuser.me/api/portraits/men/12.jpg"
+      img: "https://randomuser.me/api/portraits/men/12.jpg",
+      matricula: "0319124961"
     },
     {
       name: "Lozano Almanza Miguel Angel",
-      img: "https://randomuser.me/api/portraits/men/14.jpg"
+      img: "https://randomuser.me/api/portraits/men/14.jpg",
+      matricula: "0319124961"
     },
     {
       name: "Torrecilas Beltrán Jesús Gustavo",
-      img: "https://randomuser.me/api/portraits/men/1.jpg"
+      img: "https://randomuser.me/api/portraits/men/1.jpg",
+      matricula: "0319124961"
     },
     {
       name: "Castellanos Salgado Samuel Antonio ",
-      img: "https://randomuser.me/api/portraits/men/10.jpg"
+      img: "https://randomuser.me/api/portraits/men/10.jpg",
+      matricula: "0319124961"
+    },
+    {
+      name: "Torrecilas Beltrán Jesús Gustavo",
+      img: "https://randomuser.me/api/portraits/men/1.jpg",
+      matricula: "0319124961"
+    },
+    {
+      name: "Torrecilas Beltrán Jesús Gustavo",
+      img: "https://randomuser.me/api/portraits/men/1.jpg",
+      matricula: "0319124961"
+    },
+    {
+      name: "Torrecilas Beltrán Jesús Gustavo",
+      img: "https://randomuser.me/api/portraits/men/1.jpg",
+      matricula: "0319124961"
+    },
+    {
+      name: "Torrecilas Beltrán Jesús Gustavo",
+      img: "https://randomuser.me/api/portraits/men/1.jpg",
+      matricula: "0319124961"
+    },
+    {
+      name: "Torrecilas Beltrán Jesús Gustavo",
+      img: "https://randomuser.me/api/portraits/men/1.jpg",
+      matricula: "0319124961"
+    },
+    {
+      name: "Torrecilas Beltrán Jesús Gustavo",
+      img: "https://randomuser.me/api/portraits/men/1.jpg",
+      matricula: "0319124961"
+    },
+    {
+      name: "Torrecilas Beltrán Jesús Gustavo",
+      img: "https://randomuser.me/api/portraits/men/1.jpg",
+      matricula: "0319124961"
+    },
+    {
+      name: "Torrecilas Beltrán Jesús Gustavo",
+      img: "https://randomuser.me/api/portraits/men/1.jpg",
+      matricula: "0319124961"
     }
 
   ]
