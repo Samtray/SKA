4d084c3737cafa01e444c02798bd2d2108eccf6b
orymn <ahmed_fabian-16@hotmail.com>
2022-09-23 18:39:08 -0700

    pene
    churrendejo

--- SKA-Frontend/src/app/components/graficas/graficas.component.ts
+++ SKA-Frontend/src/app/components/graficas/graficas.component.ts
@@ -9,8 +9,24 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 export class GraficasComponent implements OnInit {
 
   constructor() { }
-
+  data:any;
   ngOnInit(): void {
+    this.data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+          {
+              label: 'First Dataset',
+              data: [65, 59, 80, 81, 56, 55, 40]
+          },
+          {
+              label: 'Second Dataset',
+              data: [28, 48, 40, 19, 86, 27, 90]
+          }
+      ]
   }
+
+  }
+
+  
 
 }
--- SKA-Frontend/src/modules/primeng/primeng.module.ts
+++ SKA-Frontend/src/modules/primeng/primeng.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CarouselModule } from 'primeng/carousel';
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
+import { ChartModule } from 'primeng/chart';
 
 
 @NgModule({
@@ -11,11 +12,13 @@ import { SidebarModule } from 'primeng/sidebar';
     CommonModule,
     CarouselModule,
     SidebarModule,
+    
   ],
   exports: [
     CarouselModule,
     ButtonModule,
-    SidebarModule
+    SidebarModule,
+    ChartModule
   ]
 })
 export class PrimengModule { }
