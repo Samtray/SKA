0e06aae01a21768ff551177244198b0e4a7aeacd
MiguelangelLozAl <miguelangellozanoa@gmail.com>
2022-10-04 19:02:12 -0700

    Endpoint id - alumno +

--- SKA-api/src/main/java/com/example/skaapi/Controllers/AlumnoController.java
+++ SKA-api/src/main/java/com/example/skaapi/Controllers/AlumnoController.java
@@ -2,12 +2,10 @@ package com.example.skaapi.Controllers;
 import com.example.skaapi.Models.Alumno.Alumno;
 import com.example.skaapi.Services.AlumnoService;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -21,6 +19,11 @@ public class AlumnoController {
     @GetMapping("alumnos")
     List<Alumno> getAlumnos(){
         return alumnoService.getAll();
+    }
+
+    @GetMapping("alumnos/{id}")
+    Optional<Alumno> getAlumno(@PathVariable String id){
+        return alumnoService.getAlumnoPerMatricula(id);
     }
 
     @PostMapping("alumnos")
