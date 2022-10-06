0e4225c669b03799eeb19687e60bfd7b9d96478c
Gtorrecillas103 <gtorrecillas103@gmail.com>
2022-10-05 19:00:32 -0700

    ya no se repite matricula

--- SKA-Frontend/src/app/services/api.service.ts
+++ SKA-Frontend/src/app/services/api.service.ts
@@ -9,7 +9,7 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   readonly apiURL = 'http://localhost:8080/api';
-  readonly gruposURL = 'https://mocki.io/v1/cbc2d3e2-ecbf-4699-9a59-37457745ef9c';
+  readonly gruposURL = 'https://mocki.io/v1/6a3e9e59-233b-48e6-8e83-6d453b8c6e07';
 
   getAlumno(matricula: string) {
     return this.http.get(this.apiURL + '/alumnos/' + matricula);
