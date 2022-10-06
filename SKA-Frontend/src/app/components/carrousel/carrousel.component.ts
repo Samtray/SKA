89d16844ab62bffa6b0a6e9c648ce5545f4d7a91
Gtorrecillas103 <gtorrecillas103@gmail.com>
2022-10-05 19:21:21 -0700

    bien perrón ya quedó

--- SKA-Frontend/src/app/components/carrousel/carrousel.component.ts
+++ SKA-Frontend/src/app/components/carrousel/carrousel.component.ts
@@ -18,7 +18,6 @@ export class CarrouselComponent implements OnInit {
     this.grupoAlumnos = value?.alumnos
   }
   grupoAlumnos: any[] = []
-  img = imgs
   selectedAlumno!: any;
   responsive = responsiveOptions;
   displayMenu: boolean = false;
@@ -69,50 +68,7 @@ export class CarrouselComponent implements OnInit {
   }
 }
 
-const imgs: any =
-  [
-    {
-      photo: "https://randomuser.me/api/portraits/men/12.jpg"
-    },
-    {
-      photo: "https://randomuser.me/api/portraits/men/14.jpg"
-    },
-    {
-      photo: "https://randomuser.me/api/portraits/men/1.jpg"
-    },
-    {
-      photo: "https://randomuser.me/api/portraits/men/10.jpg"
-    },
-    {
-      photo: "https://randomuser.me/api/portraits/men/11.jpg"
-    },
-    {
-      photo: "https://randomuser.me/api/portraits/men/13.jpg"
-    },
-    {
-      photo: "https://randomuser.me/api/portraits/men/15.jpg"
-    },
-    {
-      photo: "https://randomuser.me/api/portraits/men/16.jpg"
-    },
-    {
-      photo: "https://randomuser.me/api/portraits/men/17.jpg"
-    },
-    {
-      photo: "https://randomuser.me/api/portraits/men/19.jpg"
-    },
-    {
 
-      photo: "https://randomuser.me/api/portraits/men/20.jpg"
-    },
-    {
-      photo: "https://randomuser.me/api/portraits/men/21.jpg"
-    },
-    {
-      photo: "https://randomuser.me/api/portraits/men/22.jpg"
-    }
-
-  ]
 
 const responsiveOptions = [
   {
--- SKA-Frontend/src/app/services/api.service.ts
+++ SKA-Frontend/src/app/services/api.service.ts
@@ -9,7 +9,7 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   readonly apiURL = 'http://localhost:8080/api';
-  readonly gruposURL = 'https://mocki.io/v1/6a3e9e59-233b-48e6-8e83-6d453b8c6e07';
+  readonly gruposURL = 'https://mocki.io/v1/9c6ff20b-ce08-4305-a000-6fb5c6ce1cc5';
 
   getAlumno(matricula: string) {
     return this.http.get(this.apiURL + '/alumnos/' + matricula);
